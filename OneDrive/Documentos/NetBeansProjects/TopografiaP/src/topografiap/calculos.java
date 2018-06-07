2d14fe0d51742153505551910da36dc9d9de2a86
user <user@com1pc29>
2018-06-07 09:59:53 -0600

    creacion de clase

---
+++ OneDrive/Documentos/NetBeansProjects/TopografiaP/src/topografiap/calculos.java
@@ -0,0 +1,24 @@
+
+package topografiap;
+
+public class calculos {
+
+    public calculos() {
+    }
+    
+   public void Dobledef(double primerlec,double segunlec,double tercerleg, double cuartolec){
+   
+   
+   
+   
+   
+   
+   
+   }
+    
+    
+    
+    
+    
+    
+}
