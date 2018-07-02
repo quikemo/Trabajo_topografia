2be257503d907211116be5619d8b9aeb2bbb2dd1
Edwin Argueta <nagisaansatsu@gmail.com>
2018-07-01 20:13:34 -0600

    creada clase conexion

---
+++ TopografiaP/src/topografiap/NewClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topografiap;
+
+/**
+ *
+ * @author HP Pavilion X360
+ */
+public class NewClass {
+    //        convertir de grados minutos segundos a decimal
+        double grados, minutos, segundos;
+  /*      grados = Double.parseDouble(txtTAGrados.getText());
+        minutos= Double.parseDouble(txtTAMinutos.getText());
+        segundos= Double.parseDouble(txtTASegundos.getText());
+        minutos = minutos + (segundos/60);
+        grados= grados + (minutos/60);
+        jTextField1.setText(String.valueOf(grados));*/
+//        convertir de decimal a grados minutos y segundos
+     /*   int grad, min, seg;
+        grados = Double.parseDouble(jTextField1.getText());
+        grad= (int) grados;
+        minutos =(grados-grad)*60;
+        min=(int) minutos;
+        segundos=(minutos-min)*60;
+        seg=(int) segundos;
+        jTextField3.setText(String.valueOf(grad));
+        jTextField4.setText(String.valueOf(min));
+        jTextField5.setText(String.valueOf(seg));*/
+}
